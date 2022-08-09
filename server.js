3d011d8e16ec6ecd92cd9de8fcf7e9c621ee779f
Ram kumar <webbieram@outlook.com>
2022-08-10 00:07:27 +0530

    added middleware

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const { logger } = require('./middleware/logger');
 const PORT = process.env.PORT || 3500;
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(logger);
+// MIDDLEWARE
+// parse json request
+app.use(express.json());
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'));
 
