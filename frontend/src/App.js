f78b3cd50d1797f92233d8ced8543db2dbe2ee86
Ram kumar <4328468+ramthenmala@users.noreply.github.com>
2022-08-19 14:55:41 +0530

    updated routes without authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
+import DashLayout from "./components/DashLayout";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Public/>} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Public />} />
+          <Route path="login" element={<Login />} />
+
+          <Route path="dash" element={<DashLayout />} />
         </Route>
       </Routes>
     </div>
