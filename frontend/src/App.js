9dba1b2ba3a238688dada169b854b2b7cc13fc29
Ram kumar <4328468+ramthenmala@users.noreply.github.com>
2022-08-20 21:25:45 +0530

    updated login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
 import DashLayout from "./components/DashLayout";
-import Login from "./components/Login";
+import Login from "./features/auth/Login";
+import Welcome from "./features/auth/Welcome";
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
           <Route index element={<Public />} />
           <Route path="login" element={<Login />} />
 
-          <Route path="dash" element={<DashLayout />} />
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
+          </Route>
         </Route>
       </Routes>
     </div>
